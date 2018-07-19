03b394a85119606f189b0c376fb844fc00356e12
Jason <jvpoulos@gmail.com>
2018-07-19 19:08:02 -0400

    stampede norm run

--- funds_vae.sh
+++ funds_vae.sh
@@ -3,27 +3,27 @@
 #SBATCH -J funds_vae           # Job name
 #SBATCH -o funds_vae.o%j       # Name of stdout output file
 #SBATCH -e funds_vae.e%j       # Name of stderr error file
-#SBATCH -p development		   # Queue (partition) name
+#SBATCH -p normal		        # Queue (partition) name
 #SBATCH -N 1              		# Total # of nodes (must be 1 for serial)
 #SBATCH -n 1              	 	# Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 2:00:00        		# Run time (hh:mm:ss)
+#SBATCH -t 12:00:00        		# Run time (hh:mm:ss)
 #SBATCH --mail-user=poulos@berkeley.edu
 #SBATCH --mail-type=all    		# Send email at begin and end of job
 
-python code/train_vae.py 0 5000 'west-revpc' 'treated-gans'
-python code/train_vae.py 1 5000 'west-revpc' 'control'
+python train_vae.py 0 5000 'west-revpc' 'treated-gans'
+python train_vae.py 1 5000 'west-revpc' 'control'
 
-python code/train_vae.py 0 5000 'south-revpc' 'treated-gans'
-python code/train_vae.py 1 5000 'south-revpc' 'control'
+python train_vae.py 0 5000 'south-revpc' 'treated-gans'
+python train_vae.py 1 5000 'south-revpc' 'control'
 
-python code/train_vae.py 0 5000 'west-exppc' 'treated-gans'
-python code/train_vae.py 1 5000 'west-exppc' 'control'
+python train_vae.py 0 5000 'west-exppc' 'treated-gans'
+python train_vae.py 1 5000 'west-exppc' 'control'
 
-python code/train_vae.py 0 5000 'south-exppc' 'treated-gans'
-python code/train_vae.py 1 5000 'south-exppc' 'control'
+python train_vae.py 0 5000 'south-exppc' 'treated-gans'
+python train_vae.py 1 5000 'south-exppc' 'control'
 
-python code/train_vae.py 0 5000 'west-educpc' 'treated-gans'
-python code/train_vae.py 1 5000 'west-educpc' 'control'
+python train_vae.py 0 5000 'west-educpc' 'treated-gans'
+python train_vae.py 1 5000 'west-educpc' 'control'
 
-python code/train_vae.py 0 5000 'south-educpc' 'treated-gans'
-python code/train_vae.py 1 5000 'south-educpc' 'control'
+python train_vae.py 0 5000 'south-educpc' 'treated-gans'
+python train_vae.py 1 5000 'south-educpc' 'control'
